9a290bacbd16289432669f408f00360e63315ada
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 18:04:18 -0400

    retiro-servicio

---
+++ src/main/java/cl/karubag/retiro/RetiroServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.retiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetiroServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetiroServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/retiro/RetiroServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.retiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetiroServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
